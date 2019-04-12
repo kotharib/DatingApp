3c1b123a3b8ac175c890360a4890917dce607cfa
Brijesh Kothari <kotharibrijesh@msn.com>
2019-04-12 09:02:39 +0530

    git br

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,10 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-usaved-changes.guard';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 export function tokenGetter() {
@@ -39,7 +43,8 @@ export function tokenGetter() {
       MemberListComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -63,7 +68,10 @@ export function tokenGetter() {
       AlertifyService,
       UserService,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -7,7 +7,8 @@ namespace DatingApp.API.Models
         public int Id { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
-        public DateTime Added { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
         public bool isMain { get; set; }
         public User User { get; set; }
         public int UserId { get; set; }
